d83f3b9c7c8a85448d3e5ed3d1ab5d67bc1baadf
ssssnm <145930291+ssssnm@users.noreply.github.com>
2024-03-08 13:44:39 +0800

    Update Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -11,22 +11,27 @@ $CLONE --branch $(echo $OWRT_URL | grep -iq "lede" && echo "18.06" || echo "mast
 $CLONE --branch $(echo $OWRT_URL | grep -iq "lede" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-app-argon-config.git
 
 #Pass Wall
-$CLONE https://github.com/kenzok8/small-package.git
+#$CLONE https://github.com/kenzok8/small-package.git
 #$CLONE https://github.com/kenzok8/small.git
-#$CLONE https://github.com/xiaorouji/openwrt-passwall.git
-#$CLONE https://github.com/xiaorouji/openwrt-passwall2.git
-#$CLONE https://github.com/xiaorouji/openwrt-passwall-packages.git
+$CLONE https://github.com/xiaorouji/openwrt-passwall.git
+$CLONE https://github.com/xiaorouji/openwrt-passwall2.git
+$CLONE https://github.com/xiaorouji/openwrt-passwall-packages.git
+
 #Open Clash
 $CLONE --branch "dev" https://github.com/vernesong/OpenClash.git
+
 #Hello World
 [[ $OWRT_URL == *"lede"* ]] && $CLONE --branch "main" https://github.com/fw876/helloworld.git
+
 #Home Proxy
 [[ $OWRT_URL == *"immortalwrt"* ]] && $CLONE --branch "mdev" https://github.com/muink/homeproxy.git
 
 #adguardhome
 git clone --depth=1 --single-branch https://github.com/xiaoxiao29/luci-app-adguardhome.git
+
 #ddns-go
 #git clone --depth=1 --single-branch https://github.com/sirpdboy/luci-app-ddns-go.git
+
 #ssr-plus
 #git clone --depth=1 --single-branch https://github.com/kenzok8/small.git
 
